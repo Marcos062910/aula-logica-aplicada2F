1927e92e3d4ff1d8cc135e35e723cc208479c148
marcos062910 <marcosviniciusproencadeoliveir@gmail.com>
2023-05-18 08:28:16 -0300

    a classe que deriva as instancias,do mesmo modo que a planta é uma classe da casa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        // Aula de orientaçao á objetos
+        // Declarar objetos
+        Pessoa adao;
+        // instanciar objetos
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+         // Definiçao do formato do objeto
+        adao.nome ="Adão";
+        adao.sobrenome ="Silva";
+        eva.nome = "Eva";
+        eva.sobrenome = "silva";
+         // Acionar comportamento
+        adao.falar();
+        eva.falar();
+
+
         
     }
 }
